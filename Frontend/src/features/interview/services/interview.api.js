726c9649a76b894a452911be99eac6b9a8a3bcfb
therkchaudhary <rkchaudhary9354@gmail.com>
2026-05-28 21:14:59 +0530

    added backend live link

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL ?? (import.meta.env.DEV ? "http://localhost:3000" : "https://interview-ai-xths.onrender.com"),
     withCredentials: true,
 })
 
